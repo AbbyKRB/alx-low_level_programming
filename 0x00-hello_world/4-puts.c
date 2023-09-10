fdce8ea82b5bc8096375b9af80c94ac483dfccd5
AbirK <abirkarach98@gmail.com>
2023-09-10 12:45:12 -0700

    puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,14 @@
 #include <stdio.h>
+
 /**
  * main: entry point
+ *
+ * description: puts 
+ *
+ * return: always 0
  */
 int main(void)
 {
-	puts('/"Programming is like building a multilingual puzzle"');
-//return always success//
+	puts("\"Programming is like building a multilingual puzzle")
 	return(0);
 }
